777b23a60a94cc0feb4dfc0da92d7d1ae1d3b699
Parthapratim Bhuyan <78261370+Novice1414@users.noreply.github.com>
2023-07-20 23:09:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const colors = require("colors");
 const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
-const path = require("path");
+// const path = require("path");
 
 //dotenv
 dotenv.config();
@@ -15,7 +15,9 @@ connectDB();
 //rest object
 const app = express();
 
-const url = "https://redlifemernapp.onrender.com";
+//const url = "https://redlifemernapp.onrender.com";
+
+const link = "https://redlife.netlify.app/";
 
 //middleware
 app.use(cors());
@@ -23,17 +25,17 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
-app.use(url + "/api/v1/test", require("./routes/testRoutes"));
-app.use(url + "/api/v1/auth", require("./routes/authRoutes"));
-app.use(url + "/api/v1/inventory", require("./routes/inventoryRoutes"));
-app.use(url + "/api/v1/analytics", require("./routes/analyticsRoute"));
-app.use(url + "/api/v1/admin", require("./routes/adminRoutes"));
+app.use("/api/v1/test", require("./routes/testRoutes"));
+app.use("/api/v1/auth", require("./routes/authRoutes"));
+app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
+app.use("/api/v1/analytics", require("./routes/analyticsRoute"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
-//static files
-app.use(express.static(path.join(__dirname, "./client/build")));
+// //static files
+// app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(link);
 });
 
 //port
